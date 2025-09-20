55647ca21557c9f739acba1dbd9a72661167dcaf
aslesh <asleshashu90@gmail.com>
2025-09-20 19:18:16 +0530

    updated the cron , it willcomapare in utc format

--- frontend/my-react-app/src/config/api.js
+++ frontend/my-react-app/src/config/api.js
@@ -7,8 +7,10 @@ const API_URLS = {
 
 // Automatically detect environment
 // NODE_ENV is set by React: 'development' for npm start, 'production' for build
-const CURRENT_ENV =
-process.env.NODE_ENV === "production" ? "production" : "local";
+//const CURRENT_ENV = process.env.NODE_ENV === "production" ? "production" : "local";
+
+const CURRENT_ENV = "production";
+
 
 const API_BASE_URL = API_URLS[CURRENT_ENV];
 
